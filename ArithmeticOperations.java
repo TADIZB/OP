7467ef1925db1198b1051891e6ba0e494ea7036e
Trương Anh Đức <164633906+TADIZB@users.noreply.github.com>
2024-09-29 16:23:33 +0700

    Update ArithmeticOperations.java

--- ArithmeticOperations.java
+++ ArithmeticOperations.java
@@ -2,7 +2,7 @@ import javax.swing.JOptionPane;
 
 public class ArithmeticOperations {
     public static void main(String[] args) {
-        // Get two numbers from the user
+
         String strNum1 = JOptionPane.showInputDialog(null, "Please input the first number:", "Input the first number", JOptionPane.INFORMATION_MESSAGE);
         String strNum2 = JOptionPane.showInputDialog(null, "Please input the second number:", "Input the second number", JOptionPane.INFORMATION_MESSAGE);
         
@@ -10,12 +10,10 @@ public class ArithmeticOperations {
         double num1 = Double.parseDouble(strNum1);
         double num2 = Double.parseDouble(strNum2);
 
-        // Calculate sum, difference, and product
         double sum = num1 + num2;
         double difference = num1 - num2;
         double product = num1 * num2;
 
-        // Display the results
         String result = "Sum: " + sum + "\n" +
                         "Difference: " + difference + "\n" +
                         "Product: " + product + "\n";
@@ -28,10 +26,8 @@ public class ArithmeticOperations {
             result += "Quotient: Division by zero is not allowed";
         }
 
-        // Show the results in a message dialog
         JOptionPane.showMessageDialog(null, result, "Arithmetic Results", JOptionPane.INFORMATION_MESSAGE);
         
-        // Exit the program
         System.exit(0);
     }
 }
